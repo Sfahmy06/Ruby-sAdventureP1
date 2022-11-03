39dd43f6d285f05ad5595ef8f5b19884a3aac1ba
Sfahmy06 <116104463+Sfahmy06@users.noreply.github.com>
2022-11-03 12:21:42 -0700

    I finished the 55 tiles

--- RubysAdventureFahmy/Assets/Scriptes/RubyController.cs
+++ RubysAdventureFahmy/Assets/Scriptes/RubyController.cs
@@ -7,7 +7,6 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
     }
     // Update is called once per frame
     void Update()
